e1fca2b872168628998215351c648f036a44d436
Sam <samtbeardsley@gmail.com>
2021-03-10 13:45:09 -0600

    linked sample images in app

--- Gradio/app.py
+++ Gradio/app.py
@@ -43,7 +43,6 @@ title = 'The Food Pornographer'
 
 description = 'Upload your food photos to see what the model thinks. Are they #foodporn or #gross?'
 
-article = 'The Food Pornographer is a convolutional neural network made using transfer learning with the ResNet50 architecture. If you would like to learn more about this project you can visit my github <a href="https://github.com/samtbeardsley/Food_Porn">here</a> or drop me a line at <a href = "mailto: samtbeardsley@gmail.com">samtbeardsley@gmail.com</a>!'
-
+article = '<p>The Food Pornographer is a convolutional neural network made using transfer learning with the ResNet50 architecture. If you would like to learn more about this project you can visit my github <a href="https://github.com/samtbeardsley/Food_Porn">here</a> or drop me a line at <a href="mailto: samtbeardsley@gmail.com">samtbeardsley@gmail.com</a>! <br /><br />Need some examples? Try these:</p> <ul type="square"> <li><a href="https://github.com/samtbeardsley/Food_Porn/blob/master/Gradio/Sample%20Images/Sample%201.jpg"> sample 1 </a></li> <li><a href="https://github.com/samtbeardsley/Food_Porn/blob/master/Gradio/Sample%20Images/Sample%202.jpg"> sample 2 </a></li> <li><a href="https://github.com/samtbeardsley/Food_Porn/blob/master/Gradio/Sample%20Images/Sample%203.jpg"> sample 3 </a></li> </ul>'
 iface = gr.Interface(gradio_predict, gr.inputs.Image(), "text", title=title, description=description, article=article)
 iface.launch()
